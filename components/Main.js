caac10fc130fb478571005ad1fada0adc1f5f17f
Andy Fiedler <andyfidd@gmail.com>
2019-10-21 13:35:17 -0500

    banjo modularized the components

---
+++ components/Main.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Main.scss';
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stuff: 0,
+    };
+  }
+
+  handleButtonClick = e => {
+    e.preventDefault();
+    this.setState({ stuff: Math.floor(Math.random() * 20 + 1) });
+  };
+
+  render() {
+    return (
+      <div>
+        <h4>{this.state.stuff}</h4>
+        <button onClick={this.handleButtonClick}>Click Me</button>
+      </div>
+    );
+  }
+}
+
+
+
+
+
+
+
+
+  export default Main;
